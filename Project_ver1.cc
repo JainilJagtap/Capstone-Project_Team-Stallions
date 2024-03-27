3c0d4da1660f6accdf67ca7ed2b13d473e9b873a
ShreyShah11 <shrey111105@gmail.com>
2024-03-27 23:50:35 +0530

    added new files

---
+++ Project_ver1.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    while (1)
+    {
+        cout << "To add a task press 2" << endl;
+        cout << "To mark a task done press 3" << endl;
+        cout << "To get deadline of task with maximum priority press 4" << endl;
+        cout << "To see the deadline of all pending task press 5" << endl;
+        cout << "To exit the program press 6" << endl;
+
+        int num;
+        cin >> num;
+
+        if(num==6)
+        break;
+
+        if(num==2)
+        {
+             
+        }
+    }
+    return 0;
+}
