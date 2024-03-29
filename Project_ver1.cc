9aedbbe264014108983ed330fc77affbee0e892a
ShreyShah11 <144842677+ShreyShah11@users.noreply.github.com>
2024-03-29 22:30:51 +0530

    Added task storing and adding it to queue feature with improved priority queue and created file

--- Project_ver1.cc
+++ Project_ver1.cc
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
 #include <string>
-#define IgnoreNewChar cin.ignore()                           //this will ignore any newline character
 using namespace std;
+
+#define IgnoreNewChar cin.ignore()
 
 //Class for defining task
 class Task
 {  
-    public:
+public:
     string description;
     int priority;
-
 };
+
 class Node
 {
 public:
@@ -79,62 +79,52 @@ public:
     }
 };
 
-void addTask(string& filename){
-
-    ofstream fileOut(filename);               //opening the task file to add tasks
+void addTask(string& fileName){
+    ofstream fileOut(fileName);
     Task newTask;
-    cout<<"Enter the task description: ";
-    getline(cin,newTask.description);
-    cout<<"Enter the priority: ";
-    cin>>newTask.priority;
+    cout << "Enter the task description: ";
+    getline(cin, newTask.description);
+    cout << "Enter the priority: ";
+    cin >> newTask.priority;
     IgnoreNewChar;
 
-    //adding task to the file
     if(fileOut.is_open()){
-        fileOut<<newTask.description<<"|"<<newTask.priority<<endl;                  //here '|' separates the description and priority in file
+        fileOut << newTask.description << "|" << newTask.priority << endl;
         fileOut.close();
-        cout<<"Task successfully added to the file."<<endl;
+        cout << "Task successfully added to the file." << endl;
     }
     else{
-        cout<<"Unable to open file to write task."<<endl;
+        cout << "Unable to open file to write task." << endl;
     }
-
 }
 
-//this function loads all the tasks left in the file to the priority queue
-void FileToQueue(Priority_Queue* pq,string& filename){
-    ifstream fileIn(filename);
+void FileToQueue(PriorityQueue& pq, string& fileName){
+    ifstream fileIn(fileName);
     if(fileIn.is_open()){
         string s;
-        while(fileIn.eof()==0){
-            getline(fileIn,s);
+        while(getline(fileIn, s)){
             Task task;
             int sep;
-            for(sep=0;sep<s.size();sep++){
-                if(s[sep]=='|') break;
-                else{
-                    task.description+=s[sep];
-                }
+            for(sep = 0; sep < s.size(); sep++){
+                if(s[sep] == '|') break;
+                else task.description += s[sep];
             }
-            task.priority=s[sep+1];
-            //pushing task from file to priority queue
-            push(pq,task.description,task.priority);        //for now this push function is of priority queue which is to be reimplemented in the class
+            task.priority = stoi(s.substr(sep + 1));
+            pq.push(task.description, task.priority);
         }
         fileIn.close();
-        cout<<"Task successfully transferred from file to priority queue."<<endl;
+        cout << "Task successfully transferred from file to priority queue." << endl;
     }
     else{
-        cout<<"Unable to open file for file to queue transfer."<<endl;
+        cout << "Unable to open file for file to queue transfer." << endl;
     }
 }
 
-int main()
-{
+int main(){
+    string fileName = "Task.txt";
+    PriorityQueue pq;
 
-    
-
-    while (1)
-    {
+    while(true){
         cout << "To add a task press 2" << endl;
         cout << "To mark a task done press 3" << endl;
         cout << "To get deadline of task with maximum priority press 4" << endl;
@@ -144,23 +134,11 @@ int main()
         int num;
         cin >> num;
 
-        if (num == 6)
-            break;
+        if(num == 6) break;
 
-        if (num == 2)
-        {
-            Priority_Queue *pq;
-            pq = newNode(4, 5);
-            push(pq, 5, 3);
-            push(pq, 4, 3);
-            push(pq, 4, 5);
-            push(pq, 7, 7);
-
-            while(!isEmpty(pq))
-            {
-                cout<<peek(pq)<<" ";
-                pop(pq);
-            }
+        if(num == 2){
+            addTask(fileName);
+            FileToQueue(pq, fileName);
         }
     }
     return 0;
